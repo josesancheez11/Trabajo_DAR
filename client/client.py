56f0beb511829b6f6e15a87526a1b93e68683a6c
josesancheez11 <pepemola2019@gmail.com>
2026-03-10 10:54:51 +0100

    Implement basic TCP client for DNS queries

--- client/client.py
+++ client/client.py
@@ -1,1 +1,22 @@
+import socket
 
+SERVER_IP = "127.0.0.1"
+PORT = 5000
+
+def iniciar_cliente():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((SERVER_IP, PORT))
+
+    print("Conectado al servidor DNS")
+
+    while True:
+        mensaje = input("Introduce consulta: ")
+
+        client.sendall((mensaje + "\n").encode())
+
+        respuesta = client.recv(1024).decode()
+
+        print("Respuesta:", respuesta)
+
+if __name__ == "__main__":
+    iniciar_cliente()
